dd29c3efeb56cba05eb90239e50ec506c1042cff
whytechh <whytechh@yandex.com>
2024-11-17 00:57:10 +0500

    Update fifth_task.py

--- lab_1/fifth_task.py
+++ lab_1/fifth_task.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Чтение фрагмента, поиск таблиц
-chunk = pd.read_html('./75/fifth_task.html', attrs={'id': 'product-table'}, encoding='utf-8')
+chunk = pd.read_html('lab_1/tasks/fifth_task.html', attrs={'id': 'product-table'}, encoding='utf-8')
 
 # Выбор первой таблицы
 table = chunk[0]
 
 # Сохранение в csv-файл
-table.to_csv('./75/fifth_task_solve.csv', index=False, header=True)
+table.to_csv('lab_1/results/fifth_task_solve.csv', index=False, header=True)
